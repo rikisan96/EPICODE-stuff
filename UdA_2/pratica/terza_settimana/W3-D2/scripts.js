11cb766438a9af3ea54ea7df0264134af72d6709
rikisan96 <riccardocutruneo@gmail.com>
2024-05-07 17:04:48 +0200

    added timer

--- UdA_2/pratica/terza_settimana/W3-D2/scripts.js
+++ UdA_2/pratica/terza_settimana/W3-D2/scripts.js
@@ -11,35 +11,61 @@ const nameInput = document.getElementById("name");
 // riferimento generico al form
 const formTag = document.getElementsByTagName("form")[0];
 //creo array che conterra' i dati
-const saveButton = document.getElementById("salva")
-const delButton = document.getElementById("cancella")
+const saveButton = document.getElementById("salva");
+const delButton = document.getElementById("cancella");
+const ACButton = document.getElementById("cancellaTutto");
 
-const prevSave = document.getElementById("datoPrecedente")
+const prevSave = document.getElementById("datoPrecedente");
 
 const contact = [];
 
-// classe Contact
-class Contact {
-  constructor(_name, _phone, _fav) {
-    this.name = _name;
-  }
+formTag.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
+saveButton.addEventListener("click", function (e) {
+  newContact = nameInput.value;
+  contact.push(newContact);
+  localStorage.setItem("contact", JSON.stringify(contact));
+  console.log(`Contatto Creato!`, newContact, contact, typeof contact);
+  prevSave.innerHTML = newContact;
+  nameInput.value = "";
+});
+
+ACButton.addEventListener("click", function (e) {
+  localStorage.clear();
+  clearInterval(mioTimer);
+    hours = 0, minutes = 0, seconds.innerHTML = 0;
+});
+
+function showLastAdded(lastContact) {
+  console.log(`Contatto precedente: `, lastContact);
 }
 
-formTag.addEventListener('click', function (e){
-    e.preventDefault()
+// sezione timer
+function startCounter() {
+  let hours = parseInt(sessionStorage.getItem("hours"));
+  let minutes = parseInt(sessionStorage.getItem("minutes"));
+  let seconds = parseInt(sessionStorage.getItem("seconds"));
 
-})
+  // Aggiorna il contatore ogni secondo
+  mioTimer = setInterval(function () {
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+    sessionStorage.setItem("hours", hours);
+    sessionStorage.setItem("minutes", minutes);
+    sessionStorage.setItem("seconds", seconds);
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }, 1000);
+}
 
-
-saveButton.addEventListener('click', function (e){
-    newContact = nameInput.value
-    datoPrecedente.innerHTML = newContact
-    contact.push(newContact)
-    localStorage.setItem("contact", JSON.stringify(contact))
-    console.log(`Contatto Creato!`,newContact, contact, typeof contact)
-    nameInput.value = ""
-})
-
-delButton.addEventListener('click', function (e){
-localStorage.clear()
-})
+window.addEventListener("load", startCounter);
