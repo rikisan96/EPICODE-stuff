8aed5324c2056a19c3db3544efbf28c1b9247b35
rikisan96 <riccardocutruneo@gmail.com>
2024-05-22 16:29:56 +0200

    push esercizio d3

---
+++ UdA_3/pratica/w1_d3/esercizioD3/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ActivePostComponent } from './component/active-post/active-post.component';
+import { InactivePostComponent } from './component/inactive-post/inactive-post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ActivePostComponent,
+    InactivePostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
