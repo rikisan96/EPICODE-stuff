2306707f108c04775fc47c0692c76a5cff283e8e
rikisan96 <riccardocutruneo@gmail.com>
2024-05-23 16:35:23 +0200

    commit for update

--- UdA_3/pratica/w1_d3/esercizioD3/src/app/app-routing.module.ts
+++ UdA_3/pratica/w1_d3/esercizioD3/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ActivePostComponent } from './component/active-post/active-post.component';
+import { InactivePostComponent } from './component/inactive-post/inactive-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'active-post', component: ActivePostComponent },
+  { path: 'inactive-post', component: InactivePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
