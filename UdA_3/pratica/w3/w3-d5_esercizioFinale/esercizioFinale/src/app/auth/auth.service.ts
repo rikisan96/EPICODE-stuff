b6c78ff067c5f051d987340c4c32b650eda589af
rikisan96 <riccardocutruneo@gmail.com>
2024-06-07 16:58:03 +0200

    Commit finale finale

--- UdA_3/pratica/w3/w3-d5_esercizioFinale/esercizioFinale/src/app/auth/auth.service.ts
+++ UdA_3/pratica/w3/w3-d5_esercizioFinale/esercizioFinale/src/app/auth/auth.service.ts
@@ -1,22 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { iUser } from '../interface/user';
-//
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//
-//   isLoggedIn = false
-//
-//   constructor() { }
-//
-//   isAuthenticated() {
-//     return this.isLoggedIn;
-//   }
-//
-// }
-
-
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
@@ -31,101 +12,101 @@ import { iAuthResponse } from '../interface/i-authresponse';
 })
 export class AuthService {
 
-  jwtHelper: JwtHelperService = new JwtHelperService()//ci permette di lavorare facilmente con i jwt
+  jwtHelper: JwtHelperService = new JwtHelperService()
 
   authSubject = new BehaviorSubject<null | iUser>(null);
-  //se nel behaviorsubject c'è null significa che l'utente non è loggato, altrimenti conterrà l'oggetto user con tutte le sue info
 
-  syncIsLoggedIn: boolean = false;//Utilizziamo questa variabile all'interno dell'interceptor e delle guard per poter sapere se l'utente è loggato o meno.
 
-  user$ = this.authSubject.asObservable()//contiene i dati dell'utente loggato oppure null
+  syncIsLoggedIn: boolean = false;
+
+  user$ = this.authSubject.asObservable()
 
   isLoggedIn$ = this.user$.pipe(
     map(user => !!user),
     tap(user => this.syncIsLoggedIn = user)
-  )//restituisce true se l'utente è loggato, false se non lo è
-  //!!user è come scrivere Boolean(user)
-  //isLoggedIn$ = this.user$.pipe(map(user => Boolean(user)))
+  )
+
+
 
   constructor(
-    private http: HttpClient,//per le chiamate http
-    private router: Router//per i redirect
+    private http: HttpClient,
+    private router: Router
   ) {
 
-    this.restoreUser()//come prima cosa controllo se è già attiva una sessione, e la ripristino
+    this.restoreUser()
 
   }
 
   loginUrl: string = 'http://localhost:3000/login'
   registerUrl: string = 'http://localhost:3000/register'
 
-  //Metodo che effettua la chiamata per la registrazione. Invieremo un utente parziale poiché è il backend a definire l'id dello user.
+
   register(newUser: Partial<iUser>): Observable<iAuthResponse> {
     return this.http.post<iAuthResponse>(this.registerUrl, newUser)
-    //È necessario indicare che questa chiamata restituirà un oggetto di tipo iAuthResponse, altrimenti i componenti non potranno gestire correttamente le risposte
+
   }
 
 
-  //Metodo che effettua la chiamata al backend per effettuare un login e se questo va a buon fine, salva i dati in localStorage e li comunica al subject
+
   login(authData: iAuthData): Observable<iAuthResponse> {
     return this.http.post<iAuthResponse>(this.loginUrl, authData)
-      .pipe(tap(data => {//Tap esegue un'operazione generica ogni volta che i dati transitano all'interno di pipe
+      .pipe(tap(data => {
 
-        this.authSubject.next(data.user)//Una volta che l'utente è loggato, comunicò i suoi dati al subject.
-        localStorage.setItem('accessData', JSON.stringify(data))//A questo punto, salvo anche i dati all'interno del local storage In modo da poterli recuperare in altri momenti
+        this.authSubject.next(data.user)
+        localStorage.setItem('accessData', JSON.stringify(data))
 
-        this.autoLogout()//Nel momento in cui l'utente è loggato devo già prevedere il momento in cui verrà estromesso a causa della scadenza della sessione
+        this.autoLogout()
 
       }))
   }
 
   logout(): void {
-    //Se per fare il login comunicavamo al subject i dati dell'utente e salvavamo all'interno del local storage gli accessData in questa funzione faremo esattamente il contrario, quindi authSubject riceverà come dato null, mentre andremo ad effettuare il removeItem dei dati salvati nel local storage
 
-    this.authSubject.next(null)//Invio un dato null al subject.
-    localStorage.removeItem('accessData')//Rimuovo i dati dell'utente dal local storage.
 
-    this.router.navigate(['/auth/login'])//Sposto l'utente alla pagina di login
+    this.authSubject.next(null)
+    localStorage.removeItem('accessData')
+
+    this.router.navigate(['/auth/login'])
 
   }
 
-  //Metodo che calcola quanto tempo manca alla scadenza della sessione e avvia un timer che scateni il logout dell'utente quando questa scade
+
   autoLogout(): void {
 
-    const accessData = this.getAccessData()//Recupera i dati di accesso siccome in questa funzione devo utilizzare l'accessToken.
+    const accessData = this.getAccessData()
 
-    if (!accessData) return//Se i dati di accesso non ci sono, significa che l'utente non è loggato, quindi blocca la funzione
+    if (!accessData) return
 
-    const expDate = this.jwtHelper.getTokenExpirationDate(accessData.accessToken) as Date//trovo la data di scadenza del token
+    const expDate = this.jwtHelper.getTokenExpirationDate(accessData.accessToken) as Date
 
-    const expMs = expDate.getTime() - new Date().getTime()//Recupero Il numero di millisecondi contenuti nella data di scadenza del token e li sottraggo al numero di millisecondi relativi a data ed ora attuali. Cosi facendo ottengo il numero di millisecondi mancanti alla scadenza della sessione.
+    const expMs = expDate.getTime() - new Date().getTime()
 
-    setTimeout(this.logout, expMs)//Passato il numero di millisecondi restante avvia in automatico il logout.
+    setTimeout(this.logout, expMs)
 
   }
 
-  //Metodo utilizzabile per ottenere i dati relativi alla attuale sessione, ad esempio tutti i dati relativi all'utente eccetto la password e l'accessToken legato all'attuale sessione
+
   getAccessData(): iAuthResponse | null {
 
-    const accessDataJson = localStorage.getItem('accessData')//recupero io dati di accesso
-    if (!accessDataJson) return null //se l'utente non si è loggato i dati non ci sono, quindi blocca tutto
+    const accessDataJson = localStorage.getItem('accessData')
+    if (!accessDataJson) return null
 
-    const accessData: iAuthResponse = JSON.parse(accessDataJson)//se viene eseguita questa riga significa che i dati ci sono, quindi la converto da json ad oggetto per permetterne la manipolazione
+    const accessData: iAuthResponse = JSON.parse(accessDataJson)
 
     return accessData;
   }
 
-  //Metodo utilizzato per recuperare i dati dell'utente e ripristinarli qualora venga effettuato il reload di pagina, in mancanza di questo metodo se ricarica la pagina, l'utente si troverebbe sloggato.
+
   restoreUser(): void {
 
-    const accessData = this.getAccessData()//Recupera i dati di accesso.
+    const accessData = this.getAccessData()
 
-    if (!accessData) return//Se i dati di accesso non ci sono, significa che l'utente non è loggato, quindi blocca la funzione
+    if (!accessData) return
 
-    if (this.jwtHelper.isTokenExpired(accessData.accessToken)) return//ora controllo se il token è scaduto, se lo è fermiamo la funzione
+    if (this.jwtHelper.isTokenExpired(accessData.accessToken)) return
 
-    this.authSubject.next(accessData.user)//Se questa riga viene Letta, vuol dire che l'utente è loggato, quindi recupero i dati dell'utente e li comunico al subject per ripristinare il login
-    this.autoLogout()//Avvio il timer per l'auto logout in modo che l'utente venga estromesso quando la sessione scade
+    this.authSubject.next(accessData.user)
+    this.autoLogout()
 
   }
 
